74c7c7319aff35f5929d75b2fc301367ca0ed03f
Fernando Álisson <fernando.alisson@ufpe.br>
2025-08-03 21:00:35 -0300

    challenge 01 solved!

--- challenge_1/challenge.js
+++ challenge_1/challenge.js
@@ -1,38 +1,13 @@
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
+function printStatement(n) {
+  console.log("#".repeat(n))
 }
-
-/*
- * Complete the 'staircase' function below.
- *
- * The function accepts INTEGER n as parameter.
- */
 
 function staircase(n) {
-    // Write your code here
-
+  for (let i = 1; i <= n; i++) {
+    // Espaços: n - i
+    // '#' : i
+    const line = ' '.repeat(n - i) + '#'.repeat(i);
+    console.log(line);
+  }
 }
-
-function main() {
-    const n = parseInt(readLine().trim(), 10);
-
-    staircase(n);
-}
+staircase(10);
